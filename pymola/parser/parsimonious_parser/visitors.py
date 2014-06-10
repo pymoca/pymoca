de879c7d0a4d986f62e70ea57dabd0cf1c059917
James Goppert <james.goppert@gmail.com>
2014-06-09 22:10:04 -0400

    Fixed parsimonious printing for python 3.

--- pymola/parser/parsimonious_parser/visitors.py
+++ pymola/parser/parsimonious_parser/visitors.py
@@ -4,17 +4,17 @@ from parsimonious.nodes import NodeVisitor
 class ModelicaPrinter(NodeVisitor):
 
     def __init__(self):
-        print
+        print()
 
     def info(self, node):
         return '\033[94m {:s} \033[0m {:s}'.format(
             node.expr_name, node.full_text[node.start:node.end])
 
     def generic_visit(self, node, visited_children):
-        print self.info(node)
+        print(self.info(node))
 
     def visit_class_definition(self, node, visitied_children):
-        print self.info(node)
+        print(self.info(node))
 
     def visit_(self, node, visitied_children):
         pass
