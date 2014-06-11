709d078cc18d90eee32ac0fbd24530e315eacb07
James Goppert <james.goppert@gmail.com>
2014-06-11 13:40:42 -0400

    Removed enum dep.

--- setup.py
+++ setup.py
@@ -140,7 +140,7 @@ def setup_package():
         license='GPLv3+',
         classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
         platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
-        install_requires=['parsimonious', 'ply', 'grako', 'enum'],
+        install_requires=['parsimonious', 'ply', 'grako'],
         tests_require=['nose'],
         test_suite='nose.collector',
         packages=find_packages(
