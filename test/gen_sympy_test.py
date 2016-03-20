6615a4c6e40ea88529d2066a3eb9443f919d6b71
James Goppert <james.goppert@gmail.com>
2016-03-20 15:13:25 -0400

    Work on sympy gen.

--- test/gen_sympy_test.py
+++ test/gen_sympy_test.py
@@ -12,10 +12,37 @@ from pymola import parser
 from pymola import tree
 from pymola import gen_sympy
 
+import jinja2
+
 TEST_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class GenSympyTest(unittest.TestCase):
     "Testing"
+
+    # def test_estimator(self):
+    #     ast_tree = parser.parse(os.path.join(TEST_DIR, './Estimator.mo'))
+    #     ast_walker = tree.TreeWalker()
+    #     #flat_tree = tree.flatten(ast_tree, 'Estimator')
+    #     flat_tree = ast_tree
+    #     sympy_gen = gen_sympy.SympyGenerator()
+    #     ast_walker.walk(sympy_gen, flat_tree)
+    #     #print(flat_tree)
+    #     print(sympy_gen.src[flat_tree])
+    #     with open(os.path.join(TEST_DIR, 'generated/Estimator.py'), 'w') as f:
+    #         f.write(sympy_gen.src[flat_tree])
+    #     sys.stdout.flush()
+    #
+    # def test_aircraft(self):
+    #     ast_tree = parser.parse(os.path.join(TEST_DIR, './Aircraft.mo'))
+    #     ast_walker = tree.TreeWalker()
+    #     flat_tree = tree.flatten(ast_tree, 'Aircraft')
+    #     #sympy_gen = gen_sympy.SympyGenerator()
+    #     #ast_walker.walk(sympy_gen, flat_tree)
+    #     #print(flat_tree)
+    #     #print(sympy_gen.src[flat_tree])
+    #     #with open(os.path.join(TEST_DIR, 'generated/Aircraft.py'), 'w') as f:
+    #     #    f.write(sympy_gen.src[flat_tree])
+    #     #sys.stdout.flush()
 
     def test_estimator(self):
         ast_tree = parser.parse(os.path.join(TEST_DIR, './Estimator.mo'))
@@ -25,22 +52,11 @@ class GenSympyTest(unittest.TestCase):
         sympy_gen = gen_sympy.SympyGenerator()
         ast_walker.walk(sympy_gen, flat_tree)
         #print(flat_tree)
-        print(sympy_gen.src[flat_tree])
+        print('generated source:\n', sympy_gen.src[flat_tree])
         with open(os.path.join(TEST_DIR, 'generated/Estimator.py'), 'w') as f:
-            f.write(sympy_gen.src[flat_tree])
+           f.write(sympy_gen.src[flat_tree])
         sys.stdout.flush()
 
-    def test_aircraft(self):
-        ast_tree = parser.parse(os.path.join(TEST_DIR, './Aircraft.mo'))
-        ast_walker = tree.TreeWalker()
-        flat_tree = tree.flatten(ast_tree, 'Aircraft')
-        #sympy_gen = gen_sympy.SympyGenerator()
-        #ast_walker.walk(sympy_gen, flat_tree)
-        #print(flat_tree)
-        #print(sympy_gen.src[flat_tree])
-        #with open(os.path.join(TEST_DIR, 'generated/Aircraft.py'), 'w') as f:
-        #    f.write(sympy_gen.src[flat_tree])
-        #sys.stdout.flush()
 
 if __name__ == "__main__":
     unittest.main()
