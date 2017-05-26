2b0d33851ab6994dffaffa14c38033530e9bc082
James Goppert <james.goppert@gmail.com>
2017-05-26 04:22:21 -0400

    Switch back to 3.5 for binary antlr version.

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -2,7 +2,7 @@
 conda update -q conda
 # Useful for debugging any issues with conda
 conda info -a
-conda create -q -n pymola python=3.6 numpy scipy sympy coverage coveralls matplotlib gcc cython jupyter
+conda create -q -n pymola python=3.5 numpy scipy sympy coverage coveralls matplotlib gcc cython jupyter
 source activate pymola
 # for some reason conda install of casadi doesn't work correctly, works with pip
 # note this installs within the conda env as well since we source pymola above
