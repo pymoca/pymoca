c6c123179c3921537df6a9a8c9b7b98d3f29fb20
James Goppert <james.goppert@gmail.com>
2017-05-31 10:09:02 -0400

    Another conda setup fix.

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -e
 conda update -q conda
+conda env remove -n pymola || echo "no old pymola env found"
 # Useful for debugging any issues with conda
 conda info -a
-conda create -n pymola python=3.5
 conda config --add channels conda-forge
-conda install numpy scipy sympy coverage matplotlib gcc cython \
-	jupyter lapack pydotplus casadi coveralls
+conda create -n pymola python=3.5 numpy scipy sympy coverage \
+	matplotlib gcc cython jupyter lapack pydotplus casadi coveralls
 source activate pymola
 # for some reason conda install of casadi doesn't work correctly, works with pip
 # note this installs within the conda env as well since we source pymola above
