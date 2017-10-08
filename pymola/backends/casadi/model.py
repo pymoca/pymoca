668f92010be46d2f6c0de274861394520c2d10ee
wees <jvande42b@gmail.com>
2017-10-08 12:28:42 -0400

    get rid of old flatten() call

--- pymola/backends/casadi/model.py
+++ pymola/backends/casadi/model.py
@@ -414,9 +414,6 @@ class Model:
                 # Keep this equation
                 reduced_equations.append(eq)
 
-            # Flatten model
-            alias_rel.flatten()
-
             # Eliminate alias variables
             variables, values = [], []
             for canonical, aliases in alias_rel:
