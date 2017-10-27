pymoca/pymoca

commit d6001720d3bcff939406135077ff3fb59cb89b13

Handle value assignments like modifications
This fixes the case when we instantiate a custom type that inherits from a
built-in type.

Closes #57

pymola/parser.py
@@ -595,16 +595,15 @@ class ASTListener(ModelicaListener):
             for mod in self.ast[ctx.modification()]:
                 if isinstance(mod, ast.ClassModification):
                     sym.class_modification = mod
-                elif isinstance(mod, ast.Primary):
-                    sym.value = mod
-                elif isinstance(mod, ast.Array):
-                    sym.value = mod
-                elif isinstance(mod, ast.Expression):
-                    sym.value = mod
-                elif isinstance(mod, ast.ComponentRef):
-                    sym.value = mod
                 else:
-                    raise IOError('unhandled modification type', type(mod))
+                    # Assignment of value, which we turn into a modification here.
+                    sym_mod = ast.ClassModification()
+                    vmod_arg = ast.ClassModificationArgument()
+                    vmod_arg.value = ast.ElementModification()
+                    vmod_arg.value.component = ast.ComponentRef(name="value")
+                    vmod_arg.value.modifications = [mod]
+                    sym_mod.arguments.append(vmod_arg)
+                    sym.class_modification = sym_mod
 
     def exitElement_modification(self, ctx):
         component = self.ast[ctx.component_reference()]

pymola/tree.py
@@ -369,10 +369,6 @@ def build_instance_tree(orig_class: Union[ast.Class, ast.InstanceClass], modific
                         # modification with attribute name "value" that we
                         # pick up later in modify_symbol()
 
-                        # TODO: Is it also possible for users to say
-                        # x(nominal=1.0, value=3.0), and that x = 3.0 is
-                        # just a shorthand for x(value=3.0)
-
                         # TODO: Figure out if it's easier to directly do this
                         # in the parser.
                         vmod_arg = ast.ClassModificationArgument()

commit d4a3a6ece2517e11af4cefbc5e624f409f30122e

Fix parsing combination of mods and direct value
This bug was introduced in commit d6001720, where the (wrong) assumption
was made that combinations of symbol modifications and value assignments
were not possible. In other words, the assumption was made that
something like the following could not occur:

Real x(nominal=10.0) = 1.0;

pymola/parser.py
@@ -597,13 +597,17 @@ class ASTListener(ModelicaListener):
                     sym.class_modification = mod
                 else:
                     # Assignment of value, which we turn into a modification here.
-                    sym_mod = ast.ClassModification()
                     vmod_arg = ast.ClassModificationArgument()
                     vmod_arg.value = ast.ElementModification()
                     vmod_arg.value.component = ast.ComponentRef(name="value")
                     vmod_arg.value.modifications = [mod]
-                    sym_mod.arguments.append(vmod_arg)
-                    sym.class_modification = sym_mod
+
+                    if sym.class_modification is None:
+                        sym_mod = ast.ClassModification()
+                        sym_mod.arguments.append(vmod_arg)
+                        sym.class_modification = sym_mod
+                    else:
+                        sym.class_modification.arguments.append(vmod_arg)
 
     def exitElement_modification(self, ctx):
         component = self.ast[ctx.component_reference()]