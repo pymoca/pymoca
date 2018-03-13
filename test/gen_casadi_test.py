traversaro/pymola

Add support for caching with CasADi SXFunctions
With the support for picklable SXFunctions in CasADi 3.4.0, we can now
cache with a pickle object instead of needing code generation. With
pickling, it is possible to later still call ca.substitute() and
.expand() to simplify the graph, which can give significant performance
gains.

Code generation can still be useful for models that would not benefit
much from any later simplifcations to the graph (e.g. when there is
nothing to simplify anyway), so we will still support that with the
"codegen" option.

Note that currently caching implies expansion of the MX graph, as
pickling of MXFunctions is not yet supported.

Closes #56

test/gen_casadi_test.py
@@ -636,7 +636,30 @@ class GenCasadiTest(unittest.TestCase):
         ast_tree = parser.parse(txt)
         self.assertTrue(True)
 
-    def test_caching(self):
+    def test_cache(self):
+        # Clear cache
+        db_file = os.path.join(TEST_DIR, 'Aircraft')
+        try:
+            os.remove(db_file)
+        except:
+            pass
+
+        # Create model, cache it, and load the cache
+        compiler_options = \
+            {'cache': True}
+
+        ref_model = transfer_model(TEST_DIR, 'Aircraft', compiler_options)
+        self.assertIsInstance(ref_model, Model)
+        self.assertNotIsInstance(ref_model, CachedModel)
+
+        cached_model = transfer_model(TEST_DIR, 'Aircraft', compiler_options)
+        self.assertIsInstance(cached_model, Model)
+        self.assertIsInstance(cached_model, CachedModel)
+
+        # Compare
+        self.assert_model_equivalent_numeric(ref_model, cached_model)
+
+    def test_codegen(self):
         # Clear cache
         db_file = os.path.join(MODEL_DIR, 'Aircraft')
         try:
@@ -653,7 +676,7 @@ class GenCasadiTest(unittest.TestCase):
 
         # Create model, cache it, and load the cache
         compiler_options = \
-            {'cache': True}
+            {'codegen': True}
 
         ref_model = transfer_model(MODEL_DIR, 'Aircraft', compiler_options)
         self.assertIsInstance(ref_model, Model)

pymoca/pymoca

test/gen_casadi_test.py
@@ -636,7 +636,30 @@ class GenCasadiTest(unittest.TestCase):
         ast_tree = parser.parse(txt)
         self.assertTrue(True)
 
-    def test_caching(self):
+    def test_cache(self):
+        # Clear cache
+        db_file = os.path.join(MODEL_DIR, 'Aircraft')
+        try:
+            os.remove(db_file)
+        except:
+            pass
+
+        # Create model, cache it, and load the cache
+        compiler_options = \
+            {'cache': True}
+
+        ref_model = transfer_model(MODEL_DIR, 'Aircraft', compiler_options)
+        self.assertIsInstance(ref_model, Model)
+        self.assertNotIsInstance(ref_model, CachedModel)
+
+        cached_model = transfer_model(MODEL_DIR, 'Aircraft', compiler_options)
+        self.assertIsInstance(cached_model, Model)
+        self.assertIsInstance(cached_model, CachedModel)
+
+        # Compare
+        self.assert_model_equivalent_numeric(ref_model, cached_model)
+
+    def test_codegen(self):
         # Clear cache
         db_file = os.path.join(MODEL_DIR, 'Aircraft')
         try:
@@ -653,7 +676,7 @@ class GenCasadiTest(unittest.TestCase):
 
         # Create model, cache it, and load the cache
         compiler_options = \
-            {'cache': True}
+            {'codegen': True}
 
         ref_model = transfer_model(MODEL_DIR, 'Aircraft', compiler_options)
         self.assertIsInstance(ref_model, Model)