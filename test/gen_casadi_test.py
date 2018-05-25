pymoca/pymoca

commit 366b52b4994a4490ae4b93734e1608e69dbfcecc

Improve scalar detection in expand vectors
We can now detect a 1 by 1 matrix as a matrix, e.g. Real x[1,1]" will
result in a similarly named symbol. We however still cannot distinguish
between "Real x[1]" and "Real x", and both will result in a symbol named
"Real x" at the moment.

src/pymoca/backends/casadi/model.py
@@ -355,7 +355,7 @@ class Model:
                 old_vars = getattr(self, l)
                 new_vars = []
                 for old_var in old_vars:
-                    if old_var.symbol.numel() > 1:
+                    if old_var.symbol._modelica_shape != (1,):
                         expanded_symbols = []
                         for ind in np.ndindex(old_var.symbol._modelica_shape):
                             component_symbol = ca.MX.sym('{}[{}]'.format(old_var.symbol.name(), ",".join(str(i+1) for i in ind)))

commit 282a453417ec8e9ca5a32f896886bace087e064e

Fix expand vectors for delay and derivative states
Regression in 366b52b. We did not test for the case where we expand
vectors on scalar delay/derivative symbols. The assertion check to catch
naming mismatches ("issubset") got hit in this case, immediately pointing
to what was wrong.

src/pymoca/backends/casadi/generator.py
@@ -101,7 +101,9 @@ class Generator(TreeListener):
             if mx_symbol.is_empty():
                 continue
             if differentiate:
+                modelica_shape = mx_symbol._modelica_shape
                 mx_symbol = self.get_derivative(mx_symbol)
+                mx_symbol._modelica_shape = modelica_shape
             python_type = self.get_python_type(ast_symbol)
             variable = Variable(mx_symbol, python_type)
             if not differentiate:

src/pymoca/backends/casadi/model.py
@@ -355,7 +355,10 @@ class Model:
                 old_vars = getattr(self, l)
                 new_vars = []
                 for old_var in old_vars:
-                    if old_var.symbol._modelica_shape != (1,):
+                    # For delayed states we do not have any reliable shape
+                    # information available due to it being an arbitrary
+                    # expression, so we just always expand.
+                    if (old_var.symbol._modelica_shape != (1,) or old_var.symbol.name() in self.delay_states):
                         expanded_symbols = []
                         for ind in np.ndindex(old_var.symbol._modelica_shape):
                             component_symbol = ca.MX.sym('{}[{}]'.format(old_var.symbol.name(), ",".join(str(i+1) for i in ind)))
@@ -386,16 +389,15 @@ class Model:
                         except ValueError:
                             pass
                         else:
-                            if self.delay_arguments[i].expr.numel() > 1:
-                                delay_state = self.delay_states.pop(i)
-                                delay_argument = self.delay_arguments.pop(i)
+                            delay_state = self.delay_states.pop(i)
+                            delay_argument = self.delay_arguments.pop(i)
 
-                                for ind in np.ndindex(old_var.symbol._modelica_shape):
-                                    new_name = '{}[{}]'.format(delay_state, ",".join(str(i+1) for i in ind))
+                            for ind in np.ndindex(old_var.symbol._modelica_shape):
+                                new_name = '{}[{}]'.format(delay_state, ",".join(str(i+1) for i in ind))
 
-                                    self.delay_states.append(new_name)
-                                    self.delay_arguments.append(
-                                        DelayArgument(delay_argument.expr[ind], delay_argument.duration))
+                                self.delay_states.append(new_name)
+                                self.delay_arguments.append(
+                                    DelayArgument(delay_argument.expr[ind], delay_argument.duration))
                     else:
                         new_vars.append(old_var)
 

test/gen_casadi_test.py
@@ -597,8 +597,13 @@ class GenCasadiTest(unittest.TestCase):
         with open(os.path.join(MODEL_DIR, 'Delay.mo'), 'r') as f:
             txt = f.read()
         ast_tree = parser.parse(txt)
+
         casadi_model = gen_casadi.generate(ast_tree, 'Delay')
-        print(casadi_model)
+        # Important to also test expansion of delay expressions of scalar size
+        ast_tree = parser.parse(txt)
+        casadi_model_expanded = gen_casadi.generate(ast_tree, 'Delay')
+        casadi_model_expanded.simplify({'expand_vectors': True})
+
         ref_model = Model()
 
         x = ca.MX.sym("x")
@@ -615,6 +620,7 @@ class GenCasadiTest(unittest.TestCase):
         ref_model.delay_arguments = [DelayArgument(x, 6 * hour)]
 
         self.assert_model_equivalent_numeric(ref_model, casadi_model)
+        self.assert_model_equivalent_numeric(ref_model, casadi_model_expanded)
 
     def test_delay_for_loop(self):
         with open(os.path.join(MODEL_DIR, 'DelayForLoop.mo'), 'r') as f: