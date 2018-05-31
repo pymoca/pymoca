pymoca/pymoca

commit 901af4ccf8fb116a3f0610d171d9b88a1316bf69

Add rudimentary support for higher order arrays
This only adds support for higher ordered parameters and values, but
does not allow users to use these symbols in expressions.

Partially addresses #53

src/pymoca/backends/casadi/model.py
@@ -7,6 +7,7 @@ import re
 
 from pymoca import ast
 from .alias_relation import AliasRelation
+from .mtensor import _MTensor
 
 logger = logging.getLogger("pymoca")
 
@@ -346,36 +347,28 @@ class Model:
                 new_vars = []
                 for old_var in old_vars:
                     if old_var.symbol.numel() > 1:
-                        rows = []
-                        for i in range(old_var.symbol.size1()):
-                            cols = []
-                            for j in range(old_var.symbol.size2()):
-                                if old_var.symbol.size1() > 1 and old_var.symbol.size2() > 1:
-                                    component_symbol = ca.MX.sym('{}[{},{}]'.format(old_var.symbol.name(), i+1, j+1))
-                                elif old_var.symbol.size1() > 1:
-                                    component_symbol = ca.MX.sym('{}[{}]'.format(old_var.symbol.name(), i+1))
-                                elif old_var.symbol.size2() > 1:
-                                    component_symbol = ca.MX.sym('{}[{}]'.format(old_var.symbol.name(), j+1))
+                        expanded_symbols = []
+                        for ind in np.ndindex(old_var.symbol._modelica_shape):
+                            component_symbol = ca.MX.sym('{}[{}]'.format(old_var.symbol.name(), ",".join(str(i+1) for i in ind)))
+                            component_var = Variable(component_symbol, old_var.python_type)
+                            for attribute in CASADI_ATTRIBUTES:
+                                # Can't convert 3D arrays to MX, so we convert to nparray instead
+                                value = getattr(old_var, attribute)
+                                if not np.isscalar(value):
+                                    value = np.array(value)
                                 else:
-                                    raise AssertionError
-                                component_var = Variable(component_symbol, old_var.python_type)
-                                for attribute in CASADI_ATTRIBUTES:
                                     value = ca.MX(getattr(old_var, attribute))
-                                    if value.size1() == old_var.symbol.size1() and value.size2() == old_var.symbol.size2():
-                                        setattr(component_var, attribute, value[i, j])
-                                    elif value.size1() == old_var.symbol.size1():
-                                        setattr(component_var, attribute, value[i])
-                                    elif value.size2() == old_var.symbol.size2():
-                                        setattr(component_var, attribute, value[j])
-                                    else:
-                                        assert value.size1() == 1
-                                        assert value.size2() == 1
-                                        setattr(component_var, attribute, value)
-                                cols.append(component_var)
-                            rows.append(cols)
-                        symbols.append(old_var.symbol)
-                        values.append(ca.vertcat(*[ca.horzcat(*[v.symbol for v in row]) for row in rows]))
-                        new_vars.extend(itertools.chain.from_iterable(rows))
+
+                                if np.prod(value.shape) == 1:
+                                    setattr(component_var, attribute, value)
+                                else:
+                                    setattr(component_var, attribute, value[ind])
+                            expanded_symbols.append(component_var)
+
+                        s = old_var.symbol._mx if isinstance(old_var.symbol, _MTensor) else old_var.symbol
+                        symbols.append(s)
+                        values.append(ca.reshape(ca.vertcat(*[x.symbol for x in expanded_symbols]), *tuple(reversed(s.shape))).T)
+                        new_vars.extend(expanded_symbols)
 
                         try:
                             assert len(self.delay_states) == len(self.delay_arguments)
@@ -388,20 +381,12 @@ class Model:
                                 delay_state = self.delay_states.pop(i)
                                 delay_argument = self.delay_arguments.pop(i)
 
-                                for i in range(delay_argument.expr.size1()):
-                                    for j in range(delay_argument.expr.size2()):
-                                        if delay_argument.expr.size1() > 1 and delay_argument.expr.size2() > 1:
-                                            new_name = '{}[{},{}]'.format(delay_state, i, j)
-                                        elif delay_argument.expr.size1() > 1:
-                                            new_name = '{}[{}]'.format(delay_state, i)
-                                        elif delay_argument.expr.size2() > 1:
-                                            new_name = '{}[{}]'.format(delay_state, j)
-                                        else:
-                                            raise AssertionError
+                                for ind in np.ndindex(old_var.symbol._modelica_shape):
+                                    new_name = '{}[{}]'.format(delay_state, ",".join(str(i+1) for i in ind))
 
-                                        self.delay_states.append(new_name)
-                                        self.delay_arguments.append(
-                                            DelayArgument(delay_argument.expr[i, j], delay_argument.duration))
+                                    self.delay_states.append(new_name)
+                                    self.delay_arguments.append(
+                                        DelayArgument(delay_argument.expr[ind], delay_argument.duration))
                     else:
                         new_vars.append(old_var)
 

test/gen_casadi_test.py
@@ -183,7 +183,7 @@ class GenCasadiTest(unittest.TestCase):
                                c__up__Q + c__down__Q,
                                qa__down__Q + a__up__Q,
                                -p__Q + c__up__Q,
-                               a__down__Q + (b__up__Q + c__down__Q),   
+                               a__down__Q + (b__up__Q + c__down__Q),
                                b__down__Q + hb__up__Q,
                                zerotest__Q]
 
@@ -663,6 +663,25 @@ class GenCasadiTest(unittest.TestCase):
         ref_model.delay_arguments = [DelayArgument(3 * x_i, delay_time) for x_i in x]
 
         self.assert_model_equivalent_numeric(ref_model, casadi_model)
+
+    def test_array_3d(self):
+        casadi_model = transfer_model(MODEL_DIR, 'Array3D', {'expand_vectors': True})
+
+        target_param_values = np.array([[[ -5.326999,  54.050758,  0.000000],
+                                    [ -1.0,        0.0,       0.0]],
+                                   [[  0.000426,  -0.001241,  2.564056],
+                                    [ -1.0,        0.0,       0.0]],
+                                   [[  2.577975,  -5.203480,  0.000000],
+                                    [ -1.0,        0.0,       0.0]],
+                                   [[ 13.219650,  -3.097600, -7.551339],
+                                    [ -1.0,        0.0,       0.0]]])
+
+        param_symbols = [x.symbol for x in casadi_model.parameters]
+
+        for ind in np.ndindex(target_param_values.shape):
+            flat_index = np.ravel_multi_index(ind, target_param_values.shape)
+            self.assertEqual(param_symbols[flat_index].name(), "x[{}]".format(",".join((str(x + 1) for x in ind))))
+            self.assertEqual(casadi_model.parameters[flat_index].value, target_param_values[ind])
 
     def test_attributes(self):
         with open(os.path.join(MODEL_DIR, 'Attributes.mo'), 'r') as f:
@@ -1307,7 +1326,7 @@ class GenCasadiTest(unittest.TestCase):
         b[4] = -4
         x = ca.vertcat(x, der_x, alias, y, _tmp, cst)
         ref_model.equations = [ca.mtimes(A, x) + b]
-            
+
         # Compare
         self.assert_model_equivalent_numeric(casadi_model, ref_model)
 
@@ -1360,7 +1379,7 @@ class GenCasadiTest(unittest.TestCase):
         b[1] = -7
         x = ca.vertcat(x, der_x, y)
         ref_model.equations = [ca.mtimes(A, x) + b]
-            
+
         # Compare
         self.assert_model_equivalent_numeric(casadi_model, ref_model)
 

src/pymoca/backends/casadi/generator.py
@@ -13,6 +13,7 @@ from pymoca.tree import TreeWalker, TreeListener, flatten
 
 from .alias_relation import AliasRelation
 from .model import Model, Variable, DelayArgument
+from .mtensor import _MTensor, _new_mx
 
 logger = logging.getLogger("pymoca")
 
@@ -50,7 +51,7 @@ class ForLoop:
         step = e.step.value
         stop = self.generator.get_integer(e.stop)
         self.values = np.arange(start, stop + step, step, dtype=np.int)
-        self.index_variable = ca.MX.sym(i.name)
+        self.index_variable = _new_mx(i.name)
         self.name = i.name
         self.indexed_symbols = OrderedDict()
 
@@ -85,6 +86,9 @@ class Generator(TreeListener):
         self.map_mode = 'inline' if options.get('unroll_loops', True) else 'serial'
         self.function_mode = (True, False) if options.get('inline_functions', True) else (False, True)
         self.delay_counter = 0
+
+        # NOTE: Part of MTensor workaround.
+        self._expand_vectors_enabled = options.get('expand_vectors', False)
 
     @property
     def current_class(self):
@@ -266,7 +270,7 @@ class Generator(TreeListener):
             expr = self.get_mx(tree.operands[0])
             duration = self.get_mx(tree.operands[1])
 
-            src = ca.MX.sym('_pymoca_delay_{}'.format(self.delay_counter), *expr.size())
+            src = _new_mx('_pymoca_delay_{}'.format(self.delay_counter), *expr.size())
             self.delay_counter += 1
 
             for f in self.for_loops:
@@ -400,7 +404,7 @@ class Generator(TreeListener):
                     res = res.T
 
                     # Make the symbol with the appropriate size, and replace the old symbol with the new one.
-                    orig_symbol = ca.MX.sym(k.name(), *res.size())
+                    orig_symbol = _new_mx(k.name(), *res.size())
 
                     model_input = next(x for x in self.model.inputs if x.symbol.name() == k.name())
                     model_input.symbol = orig_symbol
@@ -591,7 +595,6 @@ class Generator(TreeListener):
     def get_symbol(self, tree):
         # Create symbol
         shape = self.get_shape(tree)
-        assert(len(shape) <= 2)
 
         if any(isinstance(x, slice) for x in shape):
             # Symbol has unspecified dimensions. Value is specified, and
@@ -614,7 +617,17 @@ class Generator(TreeListener):
 
             shape = val_shape
 
-        s = ca.MX.sym(tree.name, *shape)
+        if len(shape) > 2:
+            # MX does not support this, so we have to use our own wrapper.
+            if not self._expand_vectors_enabled:
+                raise NotImplementedError("Cannot handle 3D+ arrays without setting 'expand_vectors'")
+            s = _MTensor(tree.name, *shape)
+        else:
+            s = _new_mx(tree.name, *shape)
+
+        # Make a notion of the original shape, as MX is always 2D (even for 1D symbols).
+        s._modelica_shape = tuple(shape)
+
         self.nodes[self.current_class][tree.name] = s
         return s
 
@@ -637,7 +650,7 @@ class Generator(TreeListener):
                     else:
                         return 0
                 else:
-                    der_s = ca.MX.sym("der({})".format(s.name()), s.size())
+                    der_s = _new_mx("der({})".format(s.name()), s.size())
                     self.derivative[s.name()] = der_s
                     self.nodes[self.current_class][der_s.name()] = der_s
                     return der_s
@@ -649,7 +662,7 @@ class Generator(TreeListener):
             slice_info = s.info()['slice']
             dep = s.dep()
             if dep.name() not in self.derivative:
-                der_dep = ca.MX.sym("der({})".format(dep.name()), dep.size())
+                der_dep = _new_mx("der({})".format(dep.name()), dep.size())
                 self.derivative[dep.name()] = der_dep
                 return der_dep[slice_info['start']:slice_info['stop']:slice_info['step']]
             else:
@@ -699,10 +712,10 @@ class Generator(TreeListener):
             if isinstance(indices[0], ca.MX):
                 if len(indices) > 1:
                     s = s[:, indices[1]]
-                    indexed_symbol = ca.MX.sym('{}[{},{}]'.format(tree.name, for_loop.name, indices[1]), s.size2())
+                    indexed_symbol = _new_mx('{}[{},{}]'.format(tree.name, for_loop.name, indices[1]), s.size2())
                     index_function = lambda i : (i, indices[1])
                 else:
-                    indexed_symbol = ca.MX.sym('{}[{}]'.format(tree.name, for_loop.name))
+                    indexed_symbol = _new_mx('{}[{}]'.format(tree.name, for_loop.name))
                     index_function = lambda i : i
 
                 # If the indexed symbol is empty, we know we do not have to
@@ -711,7 +724,7 @@ class Generator(TreeListener):
                     for_loop.register_indexed_symbol(indexed_symbol, index_function, True, tree, indices[0])
             else:
                 s = ca.transpose(s[indices[0], :])
-                indexed_symbol = ca.MX.sym('{}[{},{}]'.format(tree.name, indices[0], for_loop.name), s.size2())
+                indexed_symbol = _new_mx('{}[{},{}]'.format(tree.name, indices[0], for_loop.name), s.size2())
                 index_function = lambda i: (indices[0], i)
                 if np.prod(s.shape) != 0:
                     for_loop.register_indexed_symbol(indexed_symbol, index_function, False, tree, indices[1])
@@ -740,11 +753,11 @@ class Generator(TreeListener):
 
     def get_mx(self, tree: Union[ast.Symbol, ast.ComponentRef, ast.Expression]) -> ca.MX:
         """
-        We pull components and symbols from the AST on demand.  
+        We pull components and symbols from the AST on demand.
         This is to ensure that parametrized vector dimensions can be resolved.  Vector
         dimensions need to be known at CasADi MX creation time.
-        :param tree: 
-        :return: 
+        :param tree:
+        :return:
         """
         if tree not in self.src:
             if isinstance(tree, ast.Symbol):

src/pymoca/backends/casadi/mtensor.py
@@ -1,0 +1,37 @@
+"""
+The functions and classes defined here are only supposed to be used internally
+within Pymoca to work around the <= 2D only shortcoming of CasADi. In the
+eventual generated model, everything should should be ca.MX.
+"""
+
+import casadi as ca
+
+import numpy as np
+
+
+def _new_mx(name, *shape):
+    if len(shape) == 1 and not np.isscalar(shape[0]):
+        shape = shape[0]
+
+    assert len(shape) <= 2
+
+    obj = ca.MX.sym(name, *shape)
+    obj._modelica_shape = tuple(shape)
+    return obj
+
+
+class _MTensor:
+    def __init__(self, name, *shape):
+        self._shape = tuple(shape)
+        self._mx = ca.MX.sym(name, np.prod(shape))
+
+    @property
+    def shape(self):
+        return self._shape
+
+    def __getattr__(self, attr):
+        return getattr(self._mx, attr)
+
+    def __getitem__(self, k):
+        assert len(k) == len(self._shape)
+        return self._mx[np.ravel_multi_index(tuple(k,), self._shape)]

test/models/Array3D.mo
@@ -1,0 +1,10 @@
+model Array3D
+    parameter Real x[:, :, :] = {{{ -5.326999,  54.050758,  0.000000},
+                                  { -1.0,        0.0,       0.0}},
+                                 {{  0.000426,  -0.001241,  2.564056},
+                                  { -1.0,        0.0,       0.0}},
+                                 {{  2.577975,  -5.203480,  0.000000},
+                                  { -1.0,        0.0,       0.0}},
+                                 {{ 13.219650,  -3.097600, -7.551339},
+                                  { -1.0,        0.0,       0.0}}};
+end Array3D;

commit c8676f5473693c03c593f38a868da58d07b7d01a

Fix expand vectors symbolic attribute error
This fixes a regression introduced in 901af4cc.

If a state had a shape that was not (1,) and symbolic parameters in its
attribute expressions, we would attempt to cast the sybolic expression
into a numpy array, throwing an error. We now check for symbolics in the
attribute expressions as well before casting to numpy array.

Previously, the tests did not cover this case, which is why the
regression was able to sneak in. This commit also adds a test for this
case.

src/pymoca/backends/casadi/model.py
@@ -366,7 +366,7 @@ class Model:
                             for attribute in CASADI_ATTRIBUTES:
                                 # Can't convert 3D arrays to MX, so we convert to nparray instead
                                 value = getattr(old_var, attribute)
-                                if not np.isscalar(value):
+                                if not isinstance(value, ca.MX) and not np.isscalar(value):
                                     value = np.array(value)
                                 else:
                                     value = ca.MX(getattr(old_var, attribute))

test/gen_casadi_test.py
@@ -760,7 +760,8 @@ class GenCasadiTest(unittest.TestCase):
         # Alias detection results in fmin/fmax function calls in the attributes
         compiler_options = \
             {'cache': True,
-             'detect_aliases': True}
+             'detect_aliases': True,
+             'expand_vectors': True}
 
         ref_model = transfer_model(MODEL_DIR, 'ParameterAttributes', compiler_options)
         self.assertIsInstance(ref_model, Model)

test/models/ParameterAttributes.mo
@@ -4,6 +4,7 @@ model ParameterAttributes
 	Real a(min=min_, max=10.0);
 	Real b(min=0.0,  max=max_);
 	Real c;
+	Real[3] d(each min=-max_, each max=max_);
 equation
 	a = b;
 	c = a * b;