33b2ad8e253c2d6838bdf7d8718151f93c77d5de
Jack Vreeken <jack@vreeken.me>
2018-07-04 12:04:06 +0200

    Optimize AliasRelation implementation
    For large models, especially AliasRelation's canonical_signed() can be
    called a lot (millions of times). The old implementation was slow enough
    to actually form a bottleneck. This commit optimizes the implementation
    with the following changes:
    
    - canonical_signed() now directly maps from an alias (or an inverted
      alias) to the canonical state and its sign using a dictionary.
    
    - the _aliases dict now stores both the normal and inverted aliases, no
      longer converting on the fly.
    
    - aliases() returns a normal set instead of an OrderedSet. The latter
      was only needed for canonical_signed functionality. Other than that,
      there is no particular order to aliases, so a normal set suffices.
    
    The tests results below show what effect these changes have on an
    AliasRelation with ~5000 canonical states, each having 2 positive and 2
    negative aliases.
    
    - Construction of the AliasRelation was reduced from ~650 ms to ~260 ms,
      even though the storage requirements were doubled.
    
    - The canonical_signed() and aliases() methods are anything between
      2-30x faster, depending on the argument. The table below shows the
      timings of these methods for both the old and new version, in μs per
      call.
    
      +--------------------+-----------------+------+------+---------+
      |       Method       |    Argument     | Old  | New  | Speedup |
      +--------------------+-----------------+------+------+---------+
      | canonical_signed() | canonical state |  1.5 | 0.23 |     6.5 |
      |                    | not in relation |  3.0 | 0.37 |     8.1 |
      |                    | negative alias  |  1.9 | 0.24 |     7.9 |
      |                    | positive alias  |  1.5 | 0.24 |     6.3 |
      +--------------------+-----------------+------+------+---------+
      | aliases()          | canonical state | 0.43 | 0.24 |     1.8 |
      |                    | not in relation |  1.8 | 0.26 |     6.9 |
      |                    | negative alias  |  7.8 | 0.24 |    32.5 |
      |                    | positive alias  | 0.44 | 0.24 |     1.8 |
      +--------------------+-----------------+------+------+---------+
    
      Note that the lower limit on accessing an element in a
      dictionary/OrderedDict is about 40 ns in CPython 3.6. If-else checks,
      accessing a dict as an attribute of a class (instead of a local
      reference) and call overhead result in the measured value of ~240 ns
      shown in the table above.

--- src/pymoca/backends/casadi/alias_relation.py
+++ src/pymoca/backends/casadi/alias_relation.py
@@ -1,91 +1,4 @@
-from collections import OrderedDict, MutableSet
-
-class OrderedSet(MutableSet):
-    """
-    Adapted from https://code.activestate.com/recipes/576694/
-    with some additional methods:
-    __getstate__, __setstate__, __getitem__
-    """
-
-    def __init__(self, iterable=None):
-        self.end = end = [] 
-        end += [None, end, end]         # sentinel node for doubly linked list
-        self.map = {}                   # key --> [key, prev, next]
-        if iterable is not None:
-            self |= iterable
-
-    def __len__(self):
-        return len(self.map)
-
-    def __contains__(self, key):
-        return key in self.map
-
-    def __getstate__(self):
-        """ Avoids max depth RecursionError when using pickle """
-        return list(self)
-
-    def __setstate__(self, state):
-        """ Tells pickle how to restore instance """
-        self.__init__(state)
-
-    def __getitem__(self, index):
-        if isinstance(index, slice):
-            start, stop, stride = index.indices(len(self))
-            return [self.__getitem__(i) for i in range(start, stop, stride)]
-        else:
-            end = self.end
-            curr = end[2]
-            i = 0
-            while curr is not end:
-                if i == index:
-                    return curr[0]
-                curr = curr[2]
-                i += 1
-            raise IndexError('set index {} out of range with length {}'.format(index, len(self)))
-
-    def add(self, key):
-        if key not in self.map:
-            end = self.end
-            curr = end[1]
-            curr[2] = end[1] = self.map[key] = [key, curr, end]
-
-    def discard(self, key):
-        if key in self.map:        
-            key, prev, next = self.map.pop(key)
-            prev[2] = next
-            next[1] = prev
-
-    def __iter__(self):
-        end = self.end
-        curr = end[2]
-        while curr is not end:
-            yield curr[0]
-            curr = curr[2]
-
-    def __reversed__(self):
-        end = self.end
-        curr = end[1]
-        while curr is not end:
-            yield curr[0]
-            curr = curr[1]
-
-    def pop(self, last=True):
-        if not self:
-            raise KeyError('set is empty')
-        key = self.end[1][0] if last else self.end[2][0]
-        self.discard(key)
-        return key
-
-    def __repr__(self):
-        if not self:
-            return '%s()' % (self.__class__.__name__,)
-        return '%s(%r)' % (self.__class__.__name__, list(self))
-
-    def __eq__(self, other):
-        if isinstance(other, OrderedSet):
-            return len(self) == len(other) and list(self) == list(other)
-        return set(self) == set(other)
-# End snippet
+from collections import OrderedDict
 
 
 # Code snippet from RTC-Tools, copyright Stichting Deltares, originally under the terms of the GPL
@@ -93,7 +6,8 @@ class OrderedSet(MutableSet):
 class AliasRelation:
     def __init__(self):
         self._aliases = {}
-        self._canonical_variables = OrderedSet()
+        self._canonical_variables_map = OrderedDict()
+        self._canonical_variables = set()
 
     def add(self, a, b):
         # Get the canonical names and signs
@@ -105,29 +19,24 @@ class AliasRelation:
 
         # Construct aliases (a set of equivalent variables)
         aliases = self.aliases(canonical_a)
+        inverted_aliases = self.aliases("-" + canonical_a)
         for v in self.aliases(canonical_b):
             if opposite_signs:
                 v = self.__toggle_sign(v)
             aliases.add(v)
+            inverted_aliases.add(self.__toggle_sign(v))
 
-        # Update _aliases so that keys are always positive
-        inverted_aliases = OrderedSet([self.__toggle_sign(v) for v in aliases])
         for v in aliases:
-            if self.__is_negative(v):
-                # If v is negative, we make it positive and store the inverse alias set
-                self._aliases[self.__toggle_sign(v)] = inverted_aliases
-            else:
-                self._aliases[v] = aliases
+            self._aliases[self.__toggle_sign(v)] = inverted_aliases
+            self._aliases[v] = aliases
 
         # Update _canonical_variables with new canonical var and remove old ones
-        self._canonical_variables.add(aliases[0])
-        for v in aliases[1:]:
-            if self.__is_negative(v):
-                v = self.__toggle_sign(v)
-            try:
-                self._canonical_variables.remove(v)
-            except KeyError:
-                pass
+        self._canonical_variables.add(canonical_a)
+        self._canonical_variables.discard(canonical_b)
+
+        for v in aliases:
+            self._canonical_variables_map[v] = (canonical_a, sign_a)
+            self._canonical_variables_map[self.__toggle_sign(v)] = (canonical_a, -sign_a)
 
     def __toggle_sign(self, v):
         if self.__is_negative(v):
@@ -140,31 +49,27 @@ class AliasRelation:
         return True if v[0] == '-' else False
 
     def aliases(self, a):
-        if self.__is_negative(a):
-            a = self.__toggle_sign(a)
-            return OrderedSet([self.__toggle_sign(v) for v in self.aliases(a)])
+        if a in self._aliases:
+            return self._aliases[a]
         else:
-            # Instead of the dictionary's "get" method, we avoid always
-            # instatiating an OrderedSet by explicitly checking
-            if a in self._aliases:
-                return self._aliases[a]
-            else:
-                return OrderedSet([a])
+            return {a}
 
     def canonical_signed(self, a):
-        if self.__is_negative(a):
-            top_alias = self.aliases(self.__toggle_sign(a))[0]
+        if a in self._canonical_variables_map:
+            return self._canonical_variables_map[a]
         else:
-            top_alias = self.aliases(a)[0]
-
-        if self.__is_negative(top_alias):
-            return top_alias[1:], 1 if self.__is_negative(a) else -1
-        else:
-            return top_alias, -1 if self.__is_negative(a) else 1
+            if self.__is_negative(a):
+                return a[1:], -1
+            else:
+                return a, 1
 
     @property
     def canonical_variables(self):
-        return self._canonical_variables
+        return self._canonical_variables.keys()
 
     def __iter__(self):
-        return ((canonical_variable, self.aliases(canonical_variable)[1:]) for canonical_variable in self._canonical_variables)
+        # Note that order is not guaranteed, because we are looping over a set.
+        for canonical_variable in self._canonical_variables:
+            aliases = self.aliases(canonical_variable).copy()
+            aliases.discard(canonical_variable)
+            yield canonical_variable, aliases
