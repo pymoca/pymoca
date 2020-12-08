ffbc1fbfdfbb9ecc329c6a6c5bce66ae915bb40a
Jack Vreeken <jack@vreeken.me>
2020-12-08 23:08:50 +0100

    AliasRelation: Fix handling of circular alias dependencies
    The test case that this commit adds describes the problem in a minimal
    way. The issue was caught specifically with a single circular pipe
    network (all discharges equal), which before this commit resulted in:
    
    - No canonical state of any discharge
    - All discharges were part of the algebraic states
    - All alias equations for discharges were removed from the model
    
    Basically this meant that every discharge became a free symbol, which is
    obviously very wrong. Such networks and sets of equations are not
    uncommon, and are definitely not limited to discharges (but all types of
    flow quantities).
    
    The fix is rather simple; if the alias relationship we add already
    exists, we skip it.

---
+++ test/alias_relation_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from pymoca.backends.casadi.alias_relation import AliasRelation
+
+
+class TestAliasRelation(unittest.TestCase):
+
+    def test_double_alias(self):
+        alias_relation = AliasRelation()
+
+        alias_relation.add("a", "b")
+        alias_relation.add("b", "c")
+        alias_relation.add("c", "a")
+
+        self.assertSetEqual(alias_relation.canonical_variables, {"a", })
+        self.assertSetEqual(alias_relation.aliases("a"), {"a", "b", "c"})
