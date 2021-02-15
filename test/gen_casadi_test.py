9355871878829178624fd9e05f1e146a6fadd634
Jack Vreeken <jack@vreeken.me>
2021-02-15 20:04:43 +0100

    gen_casadi_test/unittest: Fix DeprecationWarning about self.assertEquals

--- test/gen_casadi_test.py
+++ test/gen_casadi_test.py
@@ -1379,7 +1379,7 @@ class GenCasadiTest(unittest.TestCase):
 
         # Compare
         self.assert_model_equivalent_numeric(casadi_model, ref_model)
-        self.assertEquals(casadi_model.alg_states[0].aliases, {'alias'})
+        self.assertEqual(casadi_model.alg_states[0].aliases, {'alias'})
 
     def test_simplify_detect_negative_alias(self):
         # Create model, cache it, and load the cache
