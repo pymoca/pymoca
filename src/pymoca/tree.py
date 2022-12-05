4dd90480f59b408baa858bd0cea7983e6176f546
Kent Rutan <gs1150e@icloud.com>
2022-12-04 21:57:18 -0600

    Modelica: Fix symbol flattening for derived types
    This fixes an issue where symbols such as `Current` below are not
    flattened properly because they are derived from other types that
    eventually inherit from a built-in type. The resulting flat AST
    contains, for example, "a.b.__value" symbols that should have instead
    had the attributes and modifications from the "__value" symbol copied
    into the flat "a.b" symbol. The code to do that is already there, but it
    is just not catching the "__value" from "type" symbols.
    
    ```
    type ElectricCurrent = Real;
    type Current = ElectricCurrent;
    ```

--- src/pymoca/tree.py
+++ src/pymoca/tree.py
@@ -503,7 +503,10 @@ def flatten_symbols(class_: ast.InstanceClass, instance_name='') -> ast.Class:
             # Elementary type
             flat_sym.dimensions = flat_sym.dimensions
             flat_class.symbols[flat_sym.name] = flat_sym
-        elif sym.type.type == "__builtin":
+        elif sym.type.type == "__builtin" or (sym.type.type == "type"
+                                              and "__value" in sym.type.symbols
+                                              and sym.type.symbols["__value"].type.name
+                                                                    in ast.Class.BUILTIN):
             # Class inherited from elementary type (e.g. "type Voltage =
             # Real"). No flattening to be done, just copying over all
             # attributes and modifications to the class's "__value" symbol.
