e5027d7cb6658492b045bd051f0414e025c84b54
Kent Rutan <gs1150e@icloud.com>
2024-01-26 19:15:27 -0600

    Remove setup.py cruft from antlr_build.py
    We don't need setuptools stuff since antlr_build.py is now a standalone
    script.
    
    Also fixes some issues noted by linter:
    
    - ROOT_DIR is not defined if the script is imported
    
    - Use f-strings for formatting
    
    - Specify encoding for files
    
    - Use with for subprocess resources

--- antlr_build.py
+++ antlr_build.py
@@ -1,44 +1,26 @@
+"""Generate parser code from given grammar file"""
 import os
 import subprocess
 
-from setuptools import Command
-
-
-class AntlrBuildCommand(Command):
-    """Customized setuptools build command."""
-
-    user_options = []
-
-    def initialize_options(self):
-        """initialize options"""
-        pass
-
-    def finalize_options(self):
-        """finalize options"""
-        pass
-
-    def run(self):
-        "Run the build command"
-        call_antlr4("Modelica.g4")
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def call_antlr4(arg):
     "calls antlr4 on grammar file"
     # pylint: disable=unused-argument, unused-variable
     antlr_path = os.path.join(ROOT_DIR, "java", "antlr-4.13.1-complete.jar")
-    classpath = os.pathsep.join([".", "{:s}".format(antlr_path), "$CLASSPATH"])
+    classpath = os.pathsep.join([".", f"{antlr_path:s}", "$CLASSPATH"])
     generated = os.path.join(ROOT_DIR, "src", "pymoca", "generated")
     cmd = (
-        'java -Xmx500M -cp "{classpath:s}" org.antlr.v4.Tool {arg:s}'
-        " -o {generated:s} -visitor -Dlanguage=Python3".format(**locals())
+        f'java -Xmx500M -cp "{classpath:s}" org.antlr.v4.Tool {arg:s}'
+        f" -o {generated:s} -visitor -Dlanguage=Python3"
     )
     print(cmd)
-    proc = subprocess.Popen(cmd.split(), cwd=os.path.join(ROOT_DIR, "src", "pymoca"))
-    proc.communicate()
-    with open(os.path.join(generated, "__init__.py"), "w") as fid:
+    with subprocess.Popen(cmd.split(), cwd=os.path.join(ROOT_DIR, "src", "pymoca")) as proc:
+        proc.communicate()
+    with open(os.path.join(generated, "__init__.py"), "w", encoding="utf-8") as fid:
         fid.write("")
 
 
 if __name__ == "__main__":
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     call_antlr4("Modelica.g4")
